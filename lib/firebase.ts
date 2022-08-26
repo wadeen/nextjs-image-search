8a3a389175d49845752956c589d5953903ceb946
wadeen <meister.vvdo@gmail.com>
2022-08-26 13:02:28 +0900

    .

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -13,7 +13,7 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
-// const analytics = getAnalytics(app)
+const analytics = getAnalytics(app)
 
 const auth = getAuth()
 const providerGoogle = new GoogleAuthProvider()
