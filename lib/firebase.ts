075d83e0acdab0bac97b99cfd938558f84285a21
wadeen <meister.vvdo@gmail.com>
2022-08-26 17:18:53 +0900

    remove: 未使用関数削除

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 // import { getAnalytics } from 'firebase/analytics'
-import { getAuth, GoogleAuthProvider, signInAnonymously } from 'firebase/auth'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
